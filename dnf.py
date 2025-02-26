4b85f7479485e5df124f7d53bd35c9073c530c4e
jaehoon3123213 <boxstar120@gmail.com>
2025-02-26 23:02:25 +0900

    Create dnf.py

---
+++ dnf.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime, timedelta
+import json
+import pandas as pd
+import os
+api_key = os.getenv("DNF_API_KEY")
+kor_last_time = datetime.now() - timedelta(days=1) #1일 전 코드
+kor_last_time = kor_last_time.strftime("%Y%m%dT%H%M%S")
+kor_time = datetime.now()
+kor_time = kor_time.strftime("%Y%m%dT%H%M%S")
+params = {
+    "code" : "505",
+    "apikey" : api_key,
+    "limit" : 100,
+    "startDate" : kor_last_time,
+    "endDate" : kor_time
+}
+data = requests.get("https://api.neople.co.kr/df/servers/cain/characters/a7a19fdce357c1c83d48e80c66bf210a/timeline", params= params).json()
+item_date = data['timeline']['rows']
+item_data = data['timeline']['rows']
+item_data
+item_list = []
+for i in item_data:
+    b= i['data']
+    b['date'] = i['date']
+    item_list.append(b)
+df = pd.DataFrame(item_list)
+df = df.sort_values('date', ascending=True)
+if os.path.exists("item_data.csv"):
+    df.to_csv("item_data.csv", mode='a', header=False, index=False, encoding='CP949')
+else:   
+    df.to_csv("item_data.csv", mode='a', header=True, index=False, encoding='CP949')
