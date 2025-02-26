a8f133c5e4f89b2d215a4b63a3e4f613fa381219
jaehoon3123213 <boxstar120@gmail.com>
2025-02-26 23:14:04 +0900

    Update dnf.py

--- dnf.py
+++ dnf.py
@@ -4,9 +4,9 @@ import json
 import pandas as pd
 import os
 API_KEY = os.getenv("DNF_API_KEY")
-kor_last_time = datetime.now() - timedelta(days=1) #1일 전 코드
+kor_last_time = datetime.now() - timedelta(hours=16) #1일 전 코드
 kor_last_time = kor_last_time.strftime("%Y%m%dT%H%M%S")
-kor_time = datetime.now()
+kor_time = datetime.now() +timedelta(hour=8)
 kor_time = kor_time.strftime("%Y%m%dT%H%M%S")
 params = {
     "code" : "505",
